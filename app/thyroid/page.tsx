75b19dbdcaa12fa0dc9d7f0a74831f4b0d27492f
Nithish <nithishsunny1234@gmail.com>
2026-02-06 18:04:20 +0530

    removed errors

--- app/thyroid/page.tsx
+++ app/thyroid/page.tsx
@@ -15,7 +15,6 @@ const page = () => {
         rightImage="/images/thyroid/home-thyroid.webp"
         buttonText="Get In Touch"
         width="32%"
-        mobileWave="/images/home-mobile.webp"
       />
       <About
         breadcrumb="Thyroid Disorders"
